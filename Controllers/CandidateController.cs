8fb609786f8348be16d0e57199cccb475e1d57dd
mo20096258 <57130951+mo20096258@users.noreply.github.com>
2021-08-24 17:14:30 +0530

    Add project files.

---
+++ Controllers/CandidateController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntschSH.Controllers
+{
+    public class CandidateController : Controller
+    {
+        public IActionResult UpsertProfile()
+        {
+            return View();
+        }
+        public IActionResult MyProfile()
+        {
+            return View();
+        }
+    }
+}
